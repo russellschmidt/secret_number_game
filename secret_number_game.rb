b95249945f7294e09e353a4e6d2fe24dab732bde
Russell Schmidt <russell.schmidt@me.com>
2013-02-27 01:17:03 -0800

    First commit

---
+++ secret_number_game.rb
@@ -0,0 +1,13 @@
+###################
+#Secret Number Game
+#Homework #1, ITR
+#Russell Schmidt
+###################
+first_name = "Russell"
+last_name = "Schmidt"
+puts ""
+puts ""
+puts "Greetings and salutations. It is I, " + first_name + " " + last_name + " the Creator, welcoming you."
+puts "Welcoming you to Hell!!!" 
+puts "You have entered a new dimension without light and without sound." 
+puts "You have placed a wager with death itself... welcome to the Secret Number Game."
